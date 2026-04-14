67f05e9aeadd1b3ac236b7102d7b018cc5e2a162
ahmedhasan <sydhamzaovais@gmail.com>
2026-04-14 21:38:29 +0500

    Add google sheet db

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,5 +3,5 @@ export const contactsData = {
     phone: '+923014568105',
     address: 'Dysol Technologies, Lahore, Pakistan - 548000 ',
 
-    sheetAPI: ''
+    sheetAPI: 'https://sheetdb.io/api/v1/tcz8hzyemdx0r'
 }
